22a786707f90aa7476e45e248528ae04f3d91755
François Pottier <francois.pottier@inria.fr>
2025-02-07 21:14:39 +0100

    Apply the fix proposed by Opale Sjöstedt <opale.sjostedt@gmail.com>.

--- src/VisitorsCompatibility.ml
+++ src/VisitorsCompatibility.ml
@@ -56,7 +56,12 @@ let str2string (s : str) : string =
   s.txt
 
 let typ_poly (tyvars : string list) (cty : core_type) : core_type =
-  Typ.poly (List.map string2str tyvars) cty
+  (* OCaml 5.3 does not allow [Typ.poly] to be applied to an empty
+     list of universal quantifiers. *)
+  if List.is_empty tyvars then
+    cty
+  else
+    Typ.poly (List.map string2str tyvars) cty
 
 let exp_send (e : expression) (m : string) : expression =
   Exp.send e (string2str m)
